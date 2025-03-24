6dd8b3bd988da93c8f2c7deb81efb732a6376b41
unknown <xsync80@myvcl.dev>
2025-03-24 20:26:33 +0100

    Enunciado JAVA

---
+++ src/RetosProgramacion/reto03.java
@@ -0,0 +1,26 @@
+package RetosProgramacion;
+
+public class reto03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		/*
+		 * EJERCICIO:
+		 * 
+		 * Crea una agenda de contactos por terminal.
+		 * - Debes implementar funcionalidades de búsqueda, inserción, actualización
+		 *   y eliminación de contactos.
+		 * - Cada contacto debe tener un nombre y un número de teléfono.
+		 * - El programa solicita en primer lugar cuál es la operación que se quiere realizar,
+		 *   y a continuación los datos necesarios para llevarla a cabo.
+		 * - El programa no puede dejar introducir números de teléfono no numéricos y con más
+		 *   de 11 dígitos (o el número de dígitos que quieras).
+		 * - También se debe proponer una operación de finalización del programa.
+		 */
+		
+
+	}
+
+}
